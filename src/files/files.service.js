1b633be21ad65225fd3cd3a4aacda611c7aced16
Tanvir Islam <tanvir.rb@outlook.com>
2023-04-07 21:34:27 +0600

    refactor: moved schedule file delete functions to file service

--- src/files/files.service.js
+++ src/files/files.service.js
@@ -75,3 +75,25 @@ module.exports.deleteFilesByPrivateKey = async (keyName) => {
 
   return publicKeyFile === undefined && privateKeyFile === undefined;
 };
+
+/**
+ * Deletes unused files from the file system after a set time
+ * @returns {Promise<void>}
+ */
+module.exports.unusedFileDelete = async () => {
+  const directoryPath = path.join(__dirname, `../../${config.app.folder}`);
+  console.info(`Running cron job to delete unused files at: ${directoryPath}`);
+  try {
+    // Get a list of all files in the directory
+    const files = await fsPromises.readdir(directoryPath);
+    console.info(`Found ${files.length} files to delete`);
+
+    // Delete each file in the directory
+    for (const file of files) {
+      await fsPromises.unlink(`${directoryPath}/${file}`);
+      console.info(`Deleted file: ${file}`);
+    }
+  } catch (err) {
+    console.error(`Error deleting files: ${err}`);
+  }
+};
--- src/utils/unusedFileDelete.scheduler.js
+++ src/utils/unusedFileDelete.scheduler.js
@@ -1,25 +1,8 @@
 const cron = require('node-cron');
-const fsPromises = require('fs').promises;
-const path = require('path');
 const config = require('../config/config');
+const { unusedFileDelete } = require('../files/files.service');
 
-const directoryPath = path.join(__dirname, `../../${config.app.folder}`);
 const cronTime = config.app.fileCleanupCronTime;
 
 // Schedule cron job to run at the specified time
-cron.schedule(cronTime, async () => {
-  console.info(`Running cron job to delete unused files at: ${directoryPath}`);
-  try {
-    // Get a list of all files in the directory
-    const files = await fsPromises.readdir(directoryPath);
-    console.info(`Found ${files.length} files to delete`);
-
-    // Delete each file in the directory
-    for (const file of files) {
-      await fsPromises.unlink(`${directoryPath}/${file}`);
-      console.info(`Deleted file: ${file}`);
-    }
-  } catch (err) {
-    console.error(`Error deleting files: ${err}`);
-  }
-});
+cron.schedule(cronTime, unusedFileDelete);
